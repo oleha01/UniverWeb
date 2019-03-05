b8d722aca9bf79d66d949b879598557210c115fa
Олег Бутрий <oleha01@butryy.onmicrosoft.com>
2019-03-05 22:40:35 +0200

    Last commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var template = document.getElementById("review-item-template").innerHTML;
+var interpretator = Handlebars.compile(template);
+var resultHTML = interpretator({
+  array: [{
+      ImageName: "1",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "2",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "3",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "4",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "5",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "6",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "7",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "8",
+      AltText: "Alt"
+    },
+    {
+      ImageName: "9",
+      AltText: "Alt"
+    }
+  ]
+});
+document.querySelector("div.container div.gridik").innerHTML += resultHTML;
